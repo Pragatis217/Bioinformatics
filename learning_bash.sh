1071970bf8d8b129e2331cdee1b7aab93d6c1e9f
Pragatis217 <pragati.singh@wmich.edu>
2025-01-23 17:52:30 -0500

    Add files via upload

---
+++ learning_bash.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+Greetings=Hello
+Name=Pragati
+Echo "$Greetings $Name"
+Echo "This script is helping you learn bash."
+
+cd /Users/pragatisingh/Desktop
+mkdir Bioinformatics
+
+cd /Users/pragatisingh/Desktop/Bioinformatics
+mkdir bash_scripts
+
+mv /Users/pragatisingh/Desktop/bash_template.txt /Users/pragatisingh/Desktop/Bioinformatics/bash_scripts
+
+mv /Users/pragatisingh/Desktop/Bioinformatics/bash_scripts/bash_template.txt /Users/pragatisingh/Desktop/Bioinformatics/bash_scripts/bash_template.sh
+
+
+
